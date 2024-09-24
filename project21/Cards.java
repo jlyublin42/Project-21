eb05f4752869375591946477b20197a423a9ce7a
jlyublin42 <90849906+jlyublin42@users.noreply.github.com>
2024-09-24 11:11:30 -0400

    deck + card files, WIP

---
+++ project21/Cards.java
@@ -0,0 +1,147 @@
+package project21;
+
+import java.io.Serializable;
+
+public class Cards implements Serializable
+{
+    private char suit;
+    private int value;
+
+    private Cards()
+    {
+        suit = ' ';
+        value = 0;
+    }
+
+    public Cards(char newSuit, int newValue)    //assigns the suits to each value
+    {
+        if (newValue < 0 || newValue > 12)
+        {
+            this.value = newValue;
+        }
+        if (newSuit != 'H' && newSuit != 'C' && newSuit != 'D' && newSuit != 'S')
+        {
+            this.suit = newSuit;
+        }
+    }
+
+    public String toString()
+    {
+        return getSuitName() + " " + this.value;
+    }
+
+    public String getSuitName()     //giving the suits a name
+    {
+        String suit;
+
+        if (this.suit == 'H')
+        {
+            suit = "Hearts";
+        }
+        else if (this.suit == 'C')
+        {
+            suit = "Clubs";
+        }
+        else if (this.suit == 'D')
+        {
+            suit = "Diamondss";
+        }
+        else if (this.suit == 'S')
+        {
+            suit = "Spades";
+        }
+        else
+        {
+            suit = "None";
+        }
+        return suit;
+    }
+
+        public char getSuitDesignator()     //assigning the card values to their numbers in the RNG
+        {
+            return suit;
+        }
+
+        public String getNumName()
+        {
+            String name = "None";
+
+            if(this.value == 0)
+            {
+                name = "Ace";
+            }
+            if(this.value == 1)
+            {
+                name = "Two";
+            }
+            if(this.value == 2)
+            {
+                name = "Three";
+            }
+            if(this.value == 3)
+            {
+                name = "Four";
+            }
+            if(this.value == 4)
+            {
+                name = "Five";
+            }
+            if(this.value == 5)
+            {
+                name = "Six";
+            }
+            if(this.value == 6)
+            {
+                name = "Seven";
+            }
+            if(this.value == 7)
+            {
+                name = "Eight";
+            }
+            if(this.value == 8)
+            {
+                name = "Nine";
+            }
+            if(this.value == 9)
+            {
+                name = "Ten";
+            }
+            if(this.value == 10)
+            {
+                name = "Jack";
+            }
+            if(this.value == 11)
+            {
+                name = "Queen";
+            }
+            if(this.value == 12)
+            {
+                name = "King";
+            }
+
+            return name;
+        }
+
+        // encapsulation process to access hidden info. will need to make a getter/setter
+        
+        public int getNumValue()
+        {
+            return this.value;
+        }
+
+        public boolean compareSuits(Cards card)
+        {
+            return this.suit == card.getSuitDesignator();
+        }
+
+        public boolean compareValue(Cards card)
+        {
+            return this.value == card.getNumValue();
+        }
+
+        public boolean compareTo(Cards card)
+        {
+            return this.suit == card.getSuitDesignator() && this.value == card.getNumValue();
+        }
+    
+}
---
+++ project21/Deck.java
@@ -0,0 +1,36 @@
+import java.io.Serializable;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Deck extends Exception implements Serializable
+{
+
+	private int nextCardIndex;
+	
+
+	Cards[] deck = new Cards[52];
+
+	public Deck()
+    {
+
+		int count = 0;
+		try
+        {
+		for (int i = 1; i <= 13; i++) {
+			deck[count++] = new Card('H', i);
+		}
+		for (int i = 1; i <= 13; i++) {
+			deck[count++] = new Card('S', i);
+		}
+		for (int i = 1; i <= 13; i++) {
+			deck[count++] = new Card('C', i);
+		}
+		for (int i = 1; i <= 13; i++) {
+			deck[count++] = new Card('D', i);
+		}
+        Finally
+		}
+
+        nextCardIndex = 0;
+    }
+}
